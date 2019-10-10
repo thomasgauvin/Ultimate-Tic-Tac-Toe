35604e571f87eb20eb3756308d77aeb6cbd5d4ff
w.mike.brichko@gmail.com <w.mike.brichko@gmail.com>
2019-10-09 20:26:52 -0400

    it works

--- driver.py
+++ driver.py
@@ -5,23 +5,21 @@ import signal
 
 from game import Game
 
+import my_bot as bot1
+import my_bot as bot2
+
 
 @contextmanager
 def timeout(time):
     # Register a function to raise a TimeoutError on the signal.
-    signal.signal(signal.SIGALRM, raise_timeout)
+    #signal.signal(signal.SIGALRM, raise_timeout)
     # Schedule the signal to be sent after ``time``.
-    signal.alarm(time)
+    #signal.alarm(time)
 
     try:
         yield
     except TimeoutError:
         pass
-    finally:
-        # Unregister the signal so it won't be triggered
-        # if the timeout is not reached.
-        signal.signal(signal.SIGALRM, signal.SIG_IGN)
-
 
 def raise_timeout(signum, frame):
     raise TimeoutError
@@ -38,11 +36,11 @@ if __name__ == "__main__":
     # Import bot class
     game = Game()
     bots = list()
-    mod = importlib.import_module(args.bot1)
+    mod = bot1
     bot1 = getattr(mod, "bot")
     bots.append(bot1(game))
 
-    mod = importlib.import_module(args.bot2)
+    mod = bot2
     bot2 = getattr(mod, "bot")
     bots.append(bot2(game))
 
--- my_bot.py
+++ my_bot.py
@@ -8,12 +8,37 @@ This is the ONLY file you should modify.
 
 """
 
+import random
+import math
 
 class bot:
+
+
     def __init__(self, game):
+        self.map_tile = {
+            0: "NW",
+            1: "N",
+            2: "NE",
+            3: "W",
+            4: "C",
+            5: "E",
+            6: "SW",
+            7: "S",
+            8: "SE",
+        }
         self.team_name = "Foo"
+        self.game = game
 
     def move(self):
         "Logic for your bot"
+        num1 = math.floor(random.random() * 9)
+        num2 = math.floor(random.random() * 9)
 
-        return input("Enter a move (e.g. NW/SE): ").strip().split("/")
+
+        # while self.game.board[num1][num2] == "O" or self.game.board[num1][num2] == "X":
+        #     num1 = math.floor(random.random() * 9)
+        #     num2 = math.floor(random.random() * 9)
+
+        num1, num2 = self.map_tile[num1], self.map_tile[num2]
+
+        return (""+num1+"/"+num2).split("/") #cleancode
